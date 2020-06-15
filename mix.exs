0a2b6fbd3044509170be0edebbe1a73f16af3a0d
bart Lantz <bartlantz@gmail.com>
2020-06-14 19:37:28 -0600

    Adds styling for bookmarks

--- mix.exs
+++ mix.exs
@@ -46,6 +46,7 @@ defmodule Bkmarkus.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
+      {:timex, "~> 3.6"},
       {:pow, "~> 1.0.20"},
       {:plug_cowboy, "~> 2.0"}
     ]
