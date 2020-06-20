359f964263252c1b3c276d00241366e22903a4fe
bart Lantz <bartlantz@gmail.com>
2020-06-20 08:31:15 -0600

    Adds username changeset

--- lib/bkmarkus/users/user.ex
+++ lib/bkmarkus/users/user.ex
@@ -3,10 +3,21 @@ defmodule Bkmarkus.Users.User do
   use Pow.Ecto.Schema
 
   schema "users" do
-    pow_user_fields()
     field :username, :string
+    pow_user_fields()
 
     has_many :bookmarks, Bookmarkus.Bookmarks.Bookmark
     timestamps()
+  end
+
+  def changeset_username(user_or_changeset, attrs) do
+    user_or_changeset
+    |> Ecto.Changeset.cast(attrs, [:username])
+  end
+  
+  def changeset(user_or_changeset, attrs) do
+    user_or_changeset
+    |> pow_changeset(attrs)
+    |> changeset_username(attrs)
   end
 end
