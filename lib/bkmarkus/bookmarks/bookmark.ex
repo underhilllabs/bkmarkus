e33a6fff2e7d966af1f012e33bcb77f0c1a9ec70
Bart Lantz <underhilllabs@gmail.com>
2020-06-20 18:05:38 -0600

    adds parse_tags function to bookmarks schema
    should probably be moved to the Booknarks Content module

--- lib/bkmarkus/bookmarks/bookmark.ex
+++ lib/bkmarkus/bookmarks/bookmark.ex
@@ -15,10 +15,26 @@ defmodule Bkmarkus.Bookmarks.Bookmark do
     timestamps()
   end
 
-  @doc false
+  @doc """
+  Builds a changeset based on the bookmark struct and `params`.
+  """
   def changeset(bookmark, attrs) do
     bookmark
     |> cast(attrs, [:title, :address, :private, :archive_page, :description, :hashed_url, :is_archived, :archive_url, :user_id])
     |> validate_required([:title, :address, :user_id])
+    |> Ecto.Changeset.put_assoc(:tags, parse_tags(params))
+  end
+
+  defp parse_tags(params)  do
+    (params["tags"] || "")
+    |> String.split(",")
+    |> Enum.map(&String.trim/1)
+    |> Enum.reject(& &1 == "")
+    |> Enum.map(&get_or_insert_tag/1)
+  end
+
+  defp get_or_insert_tag(name) do
+    Bkmarkus.Bookmarks.Repo.get_by(Bookmarkus.Bookmarks.Tag, name: name) ||
+      Bkmarkus.Bookmarks.Repo.insert!(%Bookmarkus.Bookmarks{name: name})
   end
 end
